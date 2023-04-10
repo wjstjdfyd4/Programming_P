d66b8a118fca7b4e5b854bd50176c9b90fe9eb39
wjstjdfyd4 <127701716+wjstjdfyd4@users.noreply.github.com>
2023-04-10 10:18:07 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
-//#define DEBUG
+#define DEBUG
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
+const int MAX_STAGE_WIDTH = 30;
+const int MAX_STAGE_HEIGHT = 20;
+
+// game_state == 0 일때
 int print_title_screen()
 {
     std::cout << "******************************************" << std::endl;
@@ -24,28 +29,54 @@ int print_title_screen()
     return 0;
 }
 
-int print_introduction_screen()
-{
-    std::cout << "타이틀화면으로 돌아갈까요? (Y/N)" << std::endl;
-    return 0;
+void gotoxy(int x, int y) {
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-int print_game_board()
+int print_game_screen(int stage_width, int stage_height)
 {
-    const int BOARD_WIDTH = 10;
-    const int BOARD_HEIGHT = 10;
+    // 2차원 배열을 만들고 초기화
+    char** stage = new char* [stage_height];
+    for (int i = 0; i < stage_height; i++) {
+        stage[i] = new char[stage_width];
+        memset(stage[i], '.', stage_width);
+    }
 
-    for (int i = 0; i < BOARD_HEIGHT + 2; i++) {
-        for (int j = 0; j < BOARD_WIDTH + 2; j++) {
-            if (i == 0 || i == BOARD_HEIGHT + 1 || j == 0 || j == BOARD_WIDTH + 1) {
-                std::cout << "#";
-            }
-            else {
-                std::cout << " ";
-            }
+    // 외곽에 '#' 문자를 채우기
+    for (int i = 0; i < stage_width; i++) {
+        stage[0][i] = '#';
+        stage[stage_height - 1][i] = '#';
+    }
+    for (int i = 0; i < stage_height; i++) {
+        stage[i][0] = '#';
+        stage[i][stage_width - 1] = '#';
+    }
+
+    // 스테이터스 출력
+    for (int i = 0; i < stage_height; i++) {
+        for (int j = 0; j < stage_width; j++) {
+            std::cout << stage[i][j];
         }
         std::cout << std::endl;
     }
+
+    // 동적 할당한 메모리 반환
+    for (int i = 0; i < stage_height; i++) {
+        delete[] stage[i];
+    }
+    delete[] stage;
+
+    return 0;
+}
+
+// game_state == 2 일때
+int print_introduction_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "타이틀화면으로 돌아갈까요? (Y/N)" << std::endl;
     return 0;
 }
 
@@ -56,18 +87,17 @@ int main()
     int is_game_running = 1;
 
     while (is_game_running)
-    {   
+    {
         char key_input = '0';
         switch (game_state)
         {
         case 0:
             print_title_screen();
             key_input = _getch();
-            switch(key_input)
+            switch (key_input)
             {
             case '1':
-                print_game_board();
-                is_game_running = 0;
+                game_state = 1;
                 break;
             case '2':
                 game_state = 2;
@@ -75,6 +105,8 @@ int main()
             case '3':
                 break;
             case '4':
+                is_game_running = 0;
+                break;
             case 27:
                 is_game_running = 0;
                 break;
@@ -82,28 +114,26 @@ int main()
                 break;
             }
             break;
+        case 1:
+            print_game_screen(10, 10);
+            key_input = _getch();
         case 2:
             print_introduction_screen();
             key_input = _getch();
             switch (key_input)
             {
             case 'y':
-            case 'Y':
                 game_state = 0;
                 break;
             case 'n':
-            case 'N':
-                is_game_running = 0;
                 break;
             default:
                 break;
             }
             break;
-        
         default:
             break;
-        }     
+        }
     }
     return 0;
-} 
-
+}
